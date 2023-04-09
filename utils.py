7dec382254849eaec9e51cfe9b9c0666e08ac696
sreekaroo <sreekaroo@gmail.com>
2023-04-09 02:39:33 -0400

    models almost done

--- utils.py
+++ utils.py
@@ -12,8 +12,19 @@ import random as rnd
 import string
 from pandas import DataFrame
 from math import log2
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+
+# normalizing the pixel values ignoring the label column
+
+
+def normalizePixels(df):
+    scaler = preprocessing.StandardScaler()  # scaling with sklearn
+    ret_df = scaler.fit_transform(df)
+    # ret_df = df/255
+    return ret_df
 
 
 def dataframe_to_array(dataframe):
@@ -25,6 +36,19 @@ def dataframe_to_array(dataframe):
     inputs_array = dataframe1.iloc[:, 1:].to_numpy()
     targets_array = dataframe1['label'].to_numpy()
     return inputs_array, targets_array
+
+
+def k_folder(x_train, k):
+    """ returns list of folds. each fold is a 2 length tuple of indices: (train_indices, test_indices) """
+    n_folds = []
+    for i in range(k):
+        # splitting int o train and validation
+        split_x = np.array([j for j in range(len(x_train)) if j % k != i])
+        validation_split_x = np.array([j for j in range(len(x_train)) if j % k == i])
+
+        n_folds.append(tuple(split_x, validation_split_x))
+
+    return n_folds
 
 
 # rendering pixels in row
@@ -46,6 +70,17 @@ def do_nb(xtrain, ytrain, xtest, ytest):
     return score
 
 
+def do_logReg(xtrain, ytrain, xtest, ytest, hyperparameters={'max_iter': 3000}):
+    # fitting a basic log reg model
+
+    clf = LogisticRegression(max_iter=hyperparameters['max_iter'])
+    clf.fit(xtrain, ytrain)
+
+    score = round(clf.score(xtest, ytest), 4)
+
+    return score
+
+
 def crop_pixel_row(row, crop_size=3):
     img_grid = row_to_img_grid(row)
     img_grid = img_grid[crop_size:len(img_grid) - crop_size, crop_size:len(img_grid) - crop_size]
@@ -54,8 +89,8 @@ def crop_pixel_row(row, crop_size=3):
 
 
 # trying to crop image uniformly to increase accurcay
-def apply_crop(features, crop_size=3):
-    new_features = np.array([crop_pixel_row(row, crop_size=crop_size) for row in features])
+def apply_crop(x, crop_size=3):
+    new_features = np.array([crop_pixel_row(row, crop_size=crop_size) for row in x])
     return new_features
 
 
